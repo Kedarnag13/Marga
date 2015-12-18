66fc398c72d079111ff1bb1446fccc3b209476c3
Kedarnag Mukanahallipatna <ksatish@qwinixtech.com>
2015-12-18 22:07:23 +0530

    [development development] Adds routing, set's up the server.

--- main.go
+++ main.go
@@ -1,9 +1,20 @@
 package main
 
 import (
-	"fmt"
+	"github.com/Qwinix/rVidi-Go/api/v1/controllers/account"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
 )
 
 func main() {
-	fmt.Println("Welcome to Marga.")
+	r := mux.NewRouter()
+	// Account Routes
+	r.HandleFunc("/sign_up", account.Registration.Create).Methods("POST")
+	r.HandleFunc("/log_in", account.Session.Sign_in).Methods("POST")
+
+	http.Handle("/", r)
+	// HTTP Listening Port
+	log.Println("main : Started : Listening on: http://localhost:3000 ...")
+	log.Fatal(http.ListenAndServe("0.0.0.0:3000", nil))
 }
