39985b9a47c52a87f32d191376a415f9bf682f63
Kedarnag Mukanahallipatna <ksatish@qwinixtech.com>
2015-12-18 22:20:12 +0530

    [development development] Set's up the database.

--- api/v1/controllers/account/session.go
+++ api/v1/controllers/account/session.go
@@ -5,7 +5,15 @@ type sessionController struct{}
 var Session sessionController
 
 func (s sessionController) Create(rw http.ResponseWriter, req *http.Request) {
+	db, err := sql.Open("postgres", "password=password host=localhost dbname=marga_development sslmode=disable")
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 func (s *sessionController) Destroy(rw http.ResponseWriter, req *http.Request) {
+	db, err := sql.Open("postgres", "password=password host=localhost dbname=marga_development sslmode=disable")
+	if err != nil {
+		log.Fatal(err)
+	}
 }
--- api/v1/controllers/account/sign_up.go
+++ api/v1/controllers/account/sign_up.go
@@ -5,4 +5,17 @@ type registrationController struct{}
 var Registration registrationController
 
 func (r registrationController) Create(rw http.ResponseWriter, req *http.Request) {
+
+	body, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		panic(err)
+	}
+	err = json.Unmarshal(body, &u)
+	if err != nil {
+		panic(err)
+	}
+	db, err := sql.Open("postgres", "password=password host=localhost dbname=marga_development sslmode=disable")
+	if err != nil {
+		log.Fatal(err)
+	}
 }
