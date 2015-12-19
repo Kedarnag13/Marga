2f04dd709f75c0e0f8000cc0056e7a898efd9a92
Prakruthee <psudanandappa@qwinixtech.com>
2015-12-19 17:38:23 +0530

    [development-my_points] to give points to user

---
+++ api/v1/models/ratings.go
@@ -0,0 +1,22 @@
+package models
+
+type Mypoints struct{
+	SenderId int `valid:"numeric,required"`
+	ReciverId int `valid:"numeric,required"`
+	Points int `valid:numeric,required`
+}
+
+type ProfileErrorMessage struct {
+	Success string
+	Error   string
+}
+
+type ShowErrorMessage struct {
+	Success string
+	Error   string
+}
+
+type SuccessMessage struct {
+	Success string
+	Message string
+}
