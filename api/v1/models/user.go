0e61994a59a0bf7843957edd72240b148808bed5
Kedarnag Mukanahallipatna <ksatish@qwinixtech.com>
2015-12-19 14:56:39 +0530

    [development session_api] API for sessions.

--- api/v1/models/user.go
+++ api/v1/models/user.go
@@ -5,7 +5,7 @@ type User struct {
 	Name                  string  `valid:"alphanum,required"`
 	Username              string  `valid:"alphanum,required"`
 	Email                 string  `valid:"email"`
-	Mobile_number         int     `valid:"numeric,required"`
+	Mobile_number         string  `valid:"alphanum,required"`
 	Latitude              float64 `valid:latitude`
 	Longitude             float64 `valid:longitude`
 	Password              string  `valid:"alphanum,required"`
@@ -14,4 +14,29 @@ type User struct {
 	Devise_token          string  `valid:"alphanum,required"`
 	Ward_id               int     `valid:"numeric"`
 	Type                  string  `valid:"duck"`
+}
+
+type SignIn struct {
+	Success string
+	Message string
+	User    User
+	Session Session
+}
+
+// Session struct [account/session]
+type Session struct {
+	SessionId int
+}
+
+// Message struct [controllers/account]
+// Common for sign_up, session and password
+type Message struct {
+	Success string
+	Message string
+	User    User
+}
+
+type ErrorMessage struct {
+	Success string
+	Error   string
 }
