26839e739a1db77ed43c925ac6f6d9dca4c1f31a
Kedarnag Mukanahallipatna <ksatish@qwinixtech.com>
2016-04-29 23:35:48 +0530

    Updated Godep

--- Godeps/_workspace/src/github.com/lib/pq/doc.go
+++ Godeps/_workspace/src/github.com/lib/pq/doc.go
@@ -5,15 +5,14 @@ In most cases clients will use the database/sql package instead of
 using this package directly. For example:
 
 	import (
-		"database/sql"
-
 		_ "github.com/lib/pq"
+		"database/sql"
 	)
 
 	func main() {
 		db, err := sql.Open("postgres", "user=pqgotest dbname=pqgotest sslmode=verify-full")
 		if err != nil {
-			panic(err)
+			log.Fatal(err)
 		}
 
 		age := 21
@@ -141,34 +140,34 @@ Usage example:
 
 	txn, err := db.Begin()
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 
 	stmt, err := txn.Prepare(pq.CopyIn("users", "name", "age"))
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 
 	for _, user := range users {
 		_, err = stmt.Exec(user.Name, int64(user.Age))
 		if err != nil {
-			panic(err)
+			log.Fatal(err)
 		}
 	}
 
 	_, err = stmt.Exec()
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 
 	err = stmt.Close()
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 
 	err = txn.Commit()
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 
 
