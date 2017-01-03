cfbd2a6709ae4f99779746249648ea116a0e7b0f
Kedarnag Mukanahallipatna <ksatish@qwinix.io>
2017-01-03 21:13:17 +0530

    [development implementing_gorm] Make changes to creating sessions. Re-written the entire logic.

--- api/v1/controllers/account/session.go
+++ api/v1/controllers/account/session.go
@@ -2,14 +2,14 @@ package account
 
 import (
 	"encoding/json"
-	"github.com/Kedarnag13/Marga/api/v1/controllers"
+	// "github.com/Kedarnag13/Marga/api/v1/config/db"
+	// "github.com/Kedarnag13/Marga/api/v1/controllers"
 	"github.com/Kedarnag13/Marga/api/v1/models"
-	"github.com/Kedarnag13/Marga/api/v1/config/db"
-	"github.com/gorilla/mux"
+	// "github.com/gorilla/mux"
+	"github.com/jinzhu/gorm"
 	_ "github.com/lib/pq"
 	"io/ioutil"
 	"net/http"
-	"log"
 )
 
 type sessionController struct{}
@@ -17,25 +17,30 @@ type sessionController struct{}
 var Session sessionController
 
 func (s sessionController) Create(rw http.ResponseWriter, req *http.Request) {
+	db, err := gorm.Open("postgres", "user=postgres password=password dbname=marga_development sslmode=disable")
+	defer db.Close()
+
+	var user models.User
+	var session models.Session
+	var device models.Device
 
 	body, err := ioutil.ReadAll(req.Body)
-	var u models.User
-
-	if err != nil {
-		panic(err)
-	}
-	err = json.Unmarshal(body, &u)
 	if err != nil {
 		panic(err)
 	}
 
-	response, error, user := session(u, true, false)
+	err = json.Unmarshal(body, &user)
+	if err != nil {
+		panic(err)
+	}
 
-	if error == true {
-		log.Printf("response: %v \n", response)
-		b, err := json.Marshal(models.ErrorMessage{
-			Success: "false",
-			Error:   response,
+	var id uint
+	find_user := db.First(&user, "mobile_number = ? AND devise_token = ?", user.MobileNumber, user.DeviseToken)
+	if find_user.RecordNotFound() == true {
+		b, err := json.Marshal(models.Response{
+			Message: "",
+			Error:   "User does not exist!",
+			Success: false,
 		})
 		if err != nil {
 			panic(err)
@@ -44,174 +49,79 @@ func (s sessionController) Create(rw http.ResponseWriter, req *http.Request) {
 		rw.Write(b)
 		goto end
 	} else {
-		log.Printf("response: %v \n", response)
-		b, err := json.Marshal(models.SuccessfulSignIn{
-			Success: "true",
-			Message: "Logged in Successfully",
-			User:    user,
-			Session: models.SessionDetails{user.Id, user.Devise_token},
-		})
+		find_user := db.Table("users").Where("mobile_number = ? AND devise_token = ?", user.MobileNumber, user.DeviseToken).Select("id").Row()
+		find_user.Scan(&id)
+		check_session_exists_for_mobile_number := db.First(&session, session.User.MobileNumber)
+		if check_session_exists_for_mobile_number.RecordNotFound() == true {
+			session = models.Session{UserId: id, DeviseToken: user.DeviseToken}
+			db.Create(&session)
+			device = models.Device{UserId: id, SessionId: session.ID}
+			db.Create(&device)
 
-		if err != nil {
-			panic(err)
+			b, err := json.Marshal(models.Response{
+				Message: "Session created Successfully!",
+				Error:   "",
+				Success: true,
+			})
+
+			if err != nil {
+				panic(err)
+			}
+
+			rw.Header().Set("Content-Type", "application/json")
+			rw.Write(b)
+			goto end
+		} else {
+			b, err := json.Marshal(models.Response{
+				Message: "",
+				Error:   "Session already exists!",
+				Success: false,
+			})
+			if err != nil {
+				panic(err)
+			}
+			rw.Header().Set("Content-Type", "application/json")
+			rw.Write(b)
+			goto end
 		}
-		rw.Header().Set("Content-Type", "application/json")
-		rw.Write(b)
 	}
 end:
 }
 
-func (s *sessionController) Destroy(rw http.ResponseWriter, req *http.Request) {
+// func (s *sessionController) Destroy(rw http.ResponseWriter, req *http.Request) {
 
-	var u models.User
-	vars := mux.Vars(req)
-	devise_token := vars["devise_token"]
-	u.Devise_token = devise_token
+// 	var u models.User
+// 	vars := mux.Vars(req)
+// 	devise_token := vars["devise_token"]
+// 	u.DeviseToken = devise_token
 
-	response, error, user := session(u, false, true)
+// 	response, error, user := session(u, false, true)
 
-	if error == true {
-		log.Printf("response: %v \n", response)
-		b, err := json.Marshal(models.ErrorMessage{
-			Success: "false",
-			Error:   response,
-		})
-		if err != nil {
-			panic(err)
-		}
-		rw.Header().Set("Content-Type", "application/json")
-		rw.Write(b)
-		goto end
-	} else {
-		log.Printf("response: %v \n", response)
-		b, err := json.Marshal(models.Message{
-			User:    user,
-			Success: "true",
-			Message: response,
-		})
+// 	if error == true {
+// 		log.Printf("response: %v \n", response)
+// 		b, err := json.Marshal(models.ErrorMessage{
+// 			Success: "false",
+// 			Error:   response,
+// 		})
+// 		if err != nil {
+// 			panic(err)
+// 		}
+// 		rw.Header().Set("Content-Type", "application/json")
+// 		rw.Write(b)
+// 		goto end
+// 	} else {
+// 		log.Printf("response: %v \n", response)
+// 		b, err := json.Marshal(models.Message{
+// 			User:    user,
+// 			Success: "true",
+// 			Message: response,
+// 		})
 
-		if err != nil {
-			panic(err)
-		}
-		rw.Header().Set("Content-Type", "application/json")
-		rw.Write(b)
-	}
-end:
-}
-
-func session(user models.User, login, logout bool) (string, bool, models.User) {
-
-	get_session, err := db.DBCon.Query("SELECT * from sessions where devise_token=$1", user.Devise_token)
-	if err != nil {
-		panic(err)
-	}
-	get_user_id, err := db.DBCon.Query("SELECT id FROM users WHERE mobile_number=$1", user.Mobile_number)
-	if err != nil {
-		panic(err)
-	}
-	flag := 0
-	for get_user_id.Next() {
-		var id int
-		get_user_id.Scan(&id)
-		user_existance := controllers.Check_for_user(id)
-		if user_existance == false {
-			return "User Does not exist", true, user
-		}
-	}
-	defer get_user_id.Close()
-	if login == true {
-		if user.Mobile_number == "" || user.Password == "" {
-			return "Please enter credentials to log in", true, user
-		}
-	}
-	if user.Devise_token == "" {
-		return "Devise Token can't be empty", true, user
-	} else {
-		for get_session.Next() {
-			flag = 1
-			delete_sessions, err := db.DBCon.Prepare("DELETE from sessions where devise_token =$1")
-			if err != nil {
-				panic(err)
-			}
-			delete_sessions_res, err := delete_sessions.Exec(user.Devise_token)
-			if err != nil || delete_sessions_res == nil {
-				panic(err)
-			}
-			defer delete_sessions.Close()
-			delete_devise, err := db.DBCon.Prepare("DELETE from devices where devise_token =$1")
-			if err != nil {
-				panic(err)
-			}
-			delete_devise_res, err := delete_devise.Exec(user.Devise_token)
-			if err != nil || delete_devise_res == nil {
-				panic(err)
-			}
-			defer delete_devise.Close()
-			if logout == true {
-				user := models.User{0, "", "", "", "", 0, 0, "", "", "", user.Devise_token, ""}
-				log.Printf("Devise_token: %v\n", user.Devise_token)
-				return "Logged out Successfully", false, user
-			}
-		}
-		defer get_session.Close()
-		if logout == true && flag == 0 {
-			return "Session does not exist", true, user
-		}
-		if login == true {
-
-			get_user, err := db.DBCon.Query("SELECT id,name,username, email, mobile_number, latitude, longitude, password, password_confirmation, city, device_token, type FROM users WHERE mobile_number=$1", user.Mobile_number)
-			log.Printf("Devise_token: %v\n", user.Devise_token)
-			
-			if err != nil {
-				panic(err)
-			}
-			for get_user.Next() {
-				var id int
-				var name string
-				var username string
-				var email string
-				var mobile_number string
-				var latitude float64
-				var longitude float64
-				var password string
-				var password_confirmation string
-				var city string
-				var devise_token string
-				var user_type string
-
-				err := get_user.Scan(&id, &name, &username, &email, &mobile_number, &latitude, &longitude, &password, &password_confirmation, &city, &devise_token, &user_type)
-				if err != nil {
-					panic(err)
-				}
-				key := []byte("traveling is fun")
-				db_password := password
-				decrypt_password := controllers.Decrypt(key, db_password)
-				if mobile_number == user.Mobile_number && decrypt_password == user.Password {
-					device, err := db.DBCon.Prepare("insert into devices(devise_token,user_id)values ($1,$2)")
-					if err != nil {
-						panic(err)
-					}
-					dev_res, err := device.Exec(user.Devise_token, id)
-					if err != nil || dev_res == nil {
-						panic(err)
-					}
-					defer device.Close()
-
-					session, err := db.DBCon.Prepare("insert into sessions (user_id,devise_token) values ($1,$2)")
-					if err != nil {
-						panic(err)
-					}
-					res, err := session.Exec(id, user.Devise_token)
-					if err != nil || res == nil {
-						panic(err)
-					}
-					defer session.Close()
-					user_details := models.User{id, name, username, email, mobile_number, latitude, longitude, "", "", city, devise_token, user_type}
-					return "Logged in Successfully", false, user_details
-				}
-			}
-			defer get_user.Close()
-		}
-	}
-	return "Invalid Mobile Number or Password", true, user
-}
+// 		if err != nil {
+// 			panic(err)
+// 		}
+// 		rw.Header().Set("Content-Type", "application/json")
+// 		rw.Write(b)
+// 	}
+// end:
+// }
